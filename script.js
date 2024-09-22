b2ed331e2d737cb4e6874b2fba3ae74c38781163
DaviBackon <153544125+DaviBackon@users.noreply.github.com>
2024-09-22 09:10:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-var alternativas = document.getElementsByClassName("alternativas");
+// Obtendo todas as imagens com a classe "alternativas"
+const alternativas = document.querySelectorAll('.alternativas');
 
-for(const resposta of alternativas) {
-    
-    resposta.addEventListener('click', function(e){
-        console.log(resposta)
-        
-    })
+let mensagemElement = document.getElementById('mensagem');
+
+
+// Função para verificar a resposta e mudar a cor
+  function verificarResposta(event) {
+  const imagemClicada = event.target;
+  const respostaCorreta = imagemClicada.alt === 'cat';
+
+  // Adicionando a classe correspondente para mudar a cor
+  imagemClicada.classList.add(respostaCorreta ? 'correta' : 'errada');
+
+  // Desabilitando os cliques nas outras imagens após a resposta
+  alternativas.forEach(img => img.removeEventListener('click', verificarResposta));
+
+  if (respostaCorreta) {
+    mensagemElement.textContent = 'Parabéns! Você acertou!';
+} else {
+    mensagemElement.textContent = 'Que pena! tente outra vez!';
+}
 
 }
+
+// Adicionando o evento de clique a todas as imagens
+  alternativas.forEach(imagem => imagem.addEventListener('click', verificarResposta));
 
